7469dcac42dc7785065e77932ad2e42d60c3d9a2
songkang <songkangvip@163.com>
2018-04-28 13:53:25 +0800

    播放器全屏问题

--- app/src/main/java/com/listory/songkang/activity/MusicPlayerActivity.java
+++ app/src/main/java/com/listory/songkang/activity/MusicPlayerActivity.java
@@ -140,6 +140,17 @@ public class MusicPlayerActivity extends BaseActivity implements View.OnClickLis
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        mRootVG.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.toolbar_back:
